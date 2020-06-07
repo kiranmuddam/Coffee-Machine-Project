222aeb097c47acd1fcedc046a423ea4eb266bcfa
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-08 04:59:20 +0530

    Create project stage 4 "Coffee Machine".

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,31 +1,93 @@
 package machine;
 import java.util.Scanner;
 public class CoffeeMachine {
+    int totalMoney = 550;
+    int waterStock = 400;
+    int milkStock = 540;
+    int beansStock = 120;
+    int stockDisposableCups = 9;
+    public void displayStatsCoffeeMachine() {
+        System.out.println("The coffee machine has:");
+        System.out.println(waterStock + " of water");
+        System.out.println(milkStock + " of milk");
+        System.out.println(beansStock + " of coffee beans");
+        System.out.println(stockDisposableCups + " of disposable cups");
+        System.out.println(totalMoney + " of money");
+    }
+    public boolean checkCoffeeItemsStock(int coffeeType) {
+        if (coffeeType == 1) {
+            return waterStock >= 250 && beansStock >= 16 && stockDisposableCups >= 1;
+        } else if (coffeeType == 2) {
+            return waterStock >= 350 && milkStock >= 75 && beansStock >= 20 && stockDisposableCups >= 1;
+        } else if (coffeeType == 3) {
+            return waterStock >= 200 && milkStock >= 100 && beansStock >= 12 && stockDisposableCups >= 1;
+        }
+        return false;
+    }
+    public void buyCoffee(int coffeeType) {
+        if (coffeeType == 1 && checkCoffeeItemsStock(1)) {
+            waterStock = waterStock - 250;
+            //milkStock = milkStock - 50;
+            beansStock = beansStock - 16;
+            --stockDisposableCups;
+            totalMoney = totalMoney + 4;
+        } else if (coffeeType == 2 && checkCoffeeItemsStock(2)) {
+            waterStock = waterStock - 350;
+            milkStock = milkStock - 75;
+            beansStock = beansStock - 20;
+            --stockDisposableCups;
+            totalMoney = totalMoney + 7;
+        } else if (coffeeType == 3 && checkCoffeeItemsStock(3)) {
+            waterStock = waterStock - 200;
+            milkStock = milkStock - 100;
+            beansStock = beansStock - 12;
+            --stockDisposableCups;
+            totalMoney = totalMoney + 6;
+        }
+        System.out.println();
+        displayStatsCoffeeMachine();
+    }
+    public void fillCoffeeMachine(int waterInput, int milkInput, int beansInput, int cupsInput) {
+        waterStock = waterStock + waterInput;
+        milkStock = milkStock + milkInput;
+        beansStock = beansStock + beansInput;
+        stockDisposableCups = stockDisposableCups + cupsInput;
+        System.out.println();
+        displayStatsCoffeeMachine();
+    }
+    public void takeMoney() {
+        System.out.println("I gave you $" + totalMoney);
+        totalMoney = 0;
+        System.out.println();
+        displayStatsCoffeeMachine();
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int waterStock = scanner.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milkStock = scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int beansStock = scanner.nextInt();
-        System.out.println("Write how many cups of coffee you will need:");
-        int userInput = scanner.nextInt();
-        int MaxCoffeeMakes = Math.min(Math.min(waterStock / 200, milkStock / 50), beansStock / 15);
-        if(userInput == MaxCoffeeMakes) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if(userInput < MaxCoffeeMakes) {
-            System.out.println("Yes, I can make that amount of coffee (and even " + (MaxCoffeeMakes - userInput) + " more than that)");
-        } else if(userInput > MaxCoffeeMakes) {
-            System.out.println("No, I can make only " + (MaxCoffeeMakes) + " cup(s) of coffee");
-        }
+        CoffeeMachine myCoffeeMachine = new CoffeeMachine();
+        myCoffeeMachine.displayStatsCoffeeMachine();
+        System.out.println("Write action (buy, fill, take):");
+        String userInput = scanner.next();
+        switch (userInput) {
+            case "buy":
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino");
+                int coffeeType = scanner.nextInt();
+                myCoffeeMachine.buyCoffee(coffeeType);
+                break;
+            case "fill":
+                System.out.println("Write how many ml of water do you want to add:");
+                int waterInput = scanner.nextInt();
+                System.out.println("Write how many ml of milk do you want to add:");
+                int milkInput = scanner.nextInt();
+                System.out.println("Write how many grams of coffee beans do you want to add:");
+                int beansInput = scanner.nextInt();
+                System.out.println("Write how many disposable cups do you want to add:");
+                int cupsInput = scanner.nextInt();
+                myCoffeeMachine.fillCoffeeMachine(waterInput, milkInput, beansInput, cupsInput);
+                break;
+            case "take":
+                myCoffeeMachine.takeMoney();
+                break;
 
+        }
     }
 }
--- Coffee Machine/task/test/CoffeeMachineTest.java
+++ Coffee Machine/task/test/CoffeeMachineTest.java
@@ -3,17 +3,14 @@ import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 import org.hyperskill.hstest.testcase.TestCase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 class TestClue {
-    boolean cond;
-    int num;
-    boolean showTests;
-    TestClue(boolean c, int n, boolean showTests) {
-        cond = c;
-        num = n;
-        this.showTests = showTests;
+    String string;
+    TestClue(String s) {
+        string = s;
     }
 }
 
@@ -27,119 +24,326 @@ public class CoffeeMachineTest extends StageTest<TestClue> {
     public List<TestCase<TestClue>> generate() {
         return List.of(
             new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 0, true))
-                .setInput("300\n65\n111\n1"),
+                .setAttach(new TestClue("take\n"))
+                .setInput("take\n"),
 
             new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 2, true))
-                .setInput("600\n153\n100\n1"),
+                .setAttach(new TestClue("buy\n1\n"))
+                .setInput("buy\n1\n"),
 
             new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 2, true))
-                .setInput("1400\n150\n100\n1"),
+                .setAttach(new TestClue("buy\n2\n"))
+                .setInput("buy\n2\n"),
 
             new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 2, true))
-                .setInput("1400\n1500\n45\n1"),
+                .setAttach(new TestClue("buy\n3\n"))
+                .setInput("buy\n3\n"),
 
             new TestCase<TestClue>()
-                .setAttach(new TestClue(false, 2, true))
-                .setInput("599\n250\n200\n10"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 867, true))
-                .setInput( "345640\n43423\n23234\n1"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(false, 1548, true))
-                .setInput("345640\n434230\n23234\n19246"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(false, 172, true))
-                .setInput( "34564\n43423\n23234\n19246"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 0, false))
-                .setInput("399\n112\n111\n1"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 3, false))
-                .setInput("2400\n249\n100\n1"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 1, false))
-                .setInput("1400\n1500\n44\n1"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(false, 2, false))
-                .setInput("500\n250\n200\n10"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 171, false))
-                .setInput("34564\n43423\n23234\n1"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 1547, false))
-                .setInput("345640\n434230\n23234\n1"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(false, 868, false))
-                .setInput("345640\n43423\n23234\n19246")
-
+                .setAttach(new TestClue("fill\n2001\n510\n101\n21\n"))
+                .setInput("fill\n2001\n510\n101\n21\n")
         );
     }
 
     @Override
     public CheckResult check(String reply, TestClue clue) {
-        String[] lines = reply.trim().split(":");
-        String userOutput = lines[lines.length - 1].trim();
-        String loweredOutput = userOutput.toLowerCase();
-        boolean ans = clue.cond;
-        int amount = clue.num;
-        if (ans && loweredOutput.contains("yes")) {
-            if (amount > 0) {
-                boolean isCorrect = loweredOutput.contains(Integer.toString(amount));
-                if (isCorrect) {
-                    return CheckResult.correct();
-                } else {
+        String[] lines = reply.trim().split("\\n");
 
-                    String rightOutput =
-                        "Yes, I can make that amount of coffee" +
-                            " (and even " + amount + " more than that)";
+        if (lines.length <= 1) {
+            return CheckResult.wrong("");
+        }
 
-                    if (clue.showTests) {
-                        return new CheckResult(false,
-                            "Your output:\n" +
-                                userOutput +
-                                "\nRight output:\n" +
-                                rightOutput);
-                    } else {
-                        return CheckResult.wrong("");
-                    }
-                }
+        String[] clueLines = clue.string.trim().split("\\n");
+        String action = clueLines[0].trim();
+
+        List<Integer> milk = new ArrayList<>();
+        List<Integer> water = new ArrayList<>();
+        List<Integer> beans = new ArrayList<>();
+        List<Integer> cups = new ArrayList<>();
+        List<Integer> money = new ArrayList<>();
+
+        for (String line : lines) {
+            String[] words = line.split("\\s+");
+            if (words.length == 0) {
+                continue;
             }
-            return CheckResult.correct();
-        } else {
-            boolean cond1 = loweredOutput.contains("no");
-            boolean cond2 = loweredOutput.contains(Integer.toString(amount));
-
-            if (cond1 && cond2) {
-                return CheckResult.correct();
-            } else {
-
-                String rightOutput = "No, I can make only " +
-                    amount +" cup(s) of coffee";
-
-                if (clue.showTests) {
-                    return new CheckResult(false,
-                        "Your output:\n" +
-                        userOutput +
-                        "\nRight output:\n" +
-                        rightOutput);
-                } else {
-                    return CheckResult.wrong("");
-                }
+            String firstWord = words[0].replace("$", "");
+            int amount;
+            try {
+                amount = Integer.parseInt(firstWord);
+            }
+            catch (Exception e) {
+                continue;
+            }
+            if (line.contains("milk")) {
+                milk.add(amount);
+            }
+            else if (line.contains("water")) {
+                water.add(amount);
+            }
+            else if (line.contains("beans")) {
+                beans.add(amount);
+            }
+            else if (line.contains("cups")) {
+                cups.add(amount);
+            }
+            else if (line.contains("money")) {
+                money.add(amount);
             }
         }
+
+        if (milk.size() != 2) {
+            return new CheckResult(false,
+                "There should be two lines with \"milk\", " +
+                    "found: " + milk.size());
+        }
+
+        if (water.size() != 2) {
+            return new CheckResult(false,
+                "There should be two lines with \"water\", " +
+                    "found: " + water.size());
+        }
+
+        if (beans.size() != 2) {
+            return new CheckResult(false,
+                "There should be two lines with \"beans\", " +
+                    "found: " + beans.size());
+        }
+
+        if (cups.size() != 2) {
+            return new CheckResult(false,
+                "There should be two lines with \"cups\", " +
+                    "found: " + cups.size());
+        }
+
+        if (money.size() != 2) {
+            return new CheckResult(false,
+                "There should be two lines with \"money\", " +
+                    "found: " + money.size());
+        }
+
+
+        int milk0 = milk.get(0);
+        int milk1 = milk.get(milk.size() - 1);
+
+        int water0 = water.get(0);
+        int water1 = water.get(water.size() - 1);
+
+        int beans0 = beans.get(0);
+        int beans1 = beans.get(beans.size() - 1);
+
+        int cups0 = cups.get(0);
+        int cups1 = cups.get(cups.size() - 1);
+
+        int money0 = money.get(0);
+        int money1 = money.get(money.size() - 1);
+
+        if (water0 != 400 || milk0 != 540 || beans0 != 120
+            || cups0 != 9 || money0 != 550) {
+            return new CheckResult(false,
+                "Initial setup is wrong: " +
+                    "coffee machine should fe filled like " +
+                    "stated in the description");
+        }
+
+        int diffWater = water1 - water0;
+        int diffMilk = milk1 - milk0;
+        int diffBeans = beans1 - beans0;
+        int diffCups = cups1 - cups0;
+        int diffMoney = money1 - money0;
+
+        if (action.equals("take")) {
+
+            if (diffMilk != 0) {
+                return new CheckResult(false,
+                    "After \"take\" action milk " +
+                        "amount shouldn't be changed");
+            }
+
+            if (diffWater != 0) {
+                return new CheckResult(false,
+                    "After \"take\" action water " +
+                        "amount shouldn't be changed");
+            }
+
+            if (diffBeans != 0) {
+                return new CheckResult(false,
+                    "After \"take\" action beans " +
+                        "amount shouldn't be changed");
+            }
+
+            if (diffCups != 0) {
+                return new CheckResult(false,
+                    "After \"take\" action cups " +
+                        "amount shouldn't be changed");
+            }
+
+            if (money1 != 0) {
+                return new CheckResult(false,
+                    "After \"take\" action money " +
+                        "amount should be zero");
+            }
+
+            return CheckResult.correct();
+        }
+
+        else if (action.equals("buy")) {
+
+            String option = clueLines[1].trim();
+
+            if (option.equals("1")) {
+
+                if (diffWater != -250) {
+                    return new CheckResult(false,
+                        "After buying the first option " +
+                            "water amount should be lowered by 250");
+                }
+
+                if (diffMilk != 0) {
+                    return new CheckResult(false,
+                        "After buying the first option " +
+                            "milk amount should not be changed");
+                }
+
+                if (diffBeans != -16) {
+                    return new CheckResult(false,
+                        "After buying the first option " +
+                            "beans amount should be lowered by 16");
+                }
+
+                if (diffCups != -1) {
+                    return new CheckResult(false,
+                        "After buying the first option " +
+                            "cups amount should be lowered by 1");
+                }
+
+                if (diffMoney != 4) {
+                    return new CheckResult(false,
+                        "After buying the first option " +
+                            "money amount should be increased by 4");
+                }
+
+                return CheckResult.correct();
+
+            }
+
+            else if (option.equals("2")) {
+
+                if (diffWater != -350) {
+                    return new CheckResult(false,
+                        "After buying the second option " +
+                            "water amount should be lowered by 350");
+                }
+
+                if (diffMilk != -75) {
+                    return new CheckResult(false,
+                        "After buying the second option " +
+                            "milk amount should be lowered by 75");
+                }
+
+                if (diffBeans != -20) {
+                    return new CheckResult(false,
+                        "After buying the second option " +
+                            "beans amount should be lowered by 20");
+                }
+
+                if (diffCups != -1) {
+                    return new CheckResult(false,
+                        "After buying the second option " +
+                            "cups amount should be lowered by 1");
+                }
+
+                if (diffMoney != 7) {
+                    return new CheckResult(false,
+                        "After buying the second option " +
+                            "money amount should be increased by 7");
+                }
+
+                return CheckResult.correct();
+            }
+
+            else if (option.equals("3")) {
+
+                if (diffWater != -200) {
+                    return new CheckResult(false,
+                        "After buying the third option " +
+                            "water amount should be lowered by 350");
+                }
+
+                if (diffMilk != -100) {
+                    return new CheckResult(false,
+                        "After buying the third option " +
+                            "milk amount should be lowered by 75");
+                }
+
+                if (diffBeans != -12) {
+                    return new CheckResult(false,
+                        "After buying the third option " +
+                            "beans amount should be lowered by 20");
+                }
+
+                if (diffCups != -1) {
+                    return new CheckResult(false,
+                        "After buying the third option " +
+                            "cups amount should be lowered by 1");
+                }
+
+                if (diffMoney != 6) {
+                    return new CheckResult(false,
+                        "After buying the third option " +
+                            "money amount should be increased by 7");
+                }
+
+                return CheckResult.correct();
+            }
+        }
+
+        else if (action.equals("fill")) {
+
+            int water_ = Integer.parseInt(clueLines[1]);
+            int milk_ = Integer.parseInt(clueLines[2]);
+            int beans_ = Integer.parseInt(clueLines[3]);
+            int cups_ = Integer.parseInt(clueLines[4]);
+
+            if (diffMoney != 0) {
+                return new CheckResult(false,
+                    "After \"fill\" action " +
+                        "money amount should not be changed");
+            }
+
+            if (diffWater != water_) {
+                return new CheckResult(false,
+                    "After \"fill\" action " +
+                        "water amount expected to be increased by " + water_ +
+                        " but was increased by " + diffWater);
+            }
+
+            if (diffMilk != milk_) {
+                return new CheckResult(false,
+                    "After \"fill\" action " +
+                        "milk amount expected to be increased by " + milk_ +
+                        " but was increased by " + diffMilk);
+            }
+
+            if (diffBeans != beans_) {
+                return new CheckResult(false,
+                    "After \"fill\" action " +
+                        "beans amount expected to be increased by " + beans_ +
+                        " but was increased by " + diffBeans);
+            }
+
+            if (diffCups != cups_) {
+                return new CheckResult(false,
+                    "After \"fill\" action " +
+                        "cups amount expected to be increased by " + cups_ +
+                        " but was increased by " + diffCups);
+            }
+
+
+            return CheckResult.correct();
+
+        }
+
+        throw new RuntimeException("Can't check the answer");
     }
 }
