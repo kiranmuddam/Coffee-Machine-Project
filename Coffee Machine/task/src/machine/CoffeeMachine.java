9602c0202f8eb81c7a4ba0737247349856b42514
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-07 12:08:29 +0530

    Create project stage 2 "Coffee Machine".

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,7 +1,8 @@
 package machine;
-
+import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
         System.out.println("Boiling water");
@@ -9,5 +10,11 @@ public class CoffeeMachine {
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");
+        System.out.println("Write how many cups of coffee you will need:");
+        int userInput = scanner.nextInt();
+        System.out.println("For "+ userInput + " cups of coffee you will need:");
+        System.out.println(200 * userInput + " ml of water");
+        System.out.println(50 * userInput + " ml of milk");
+        System.out.println(15 * userInput + " g of coffee beans");
     }
 }
